144ebdaf590a893537291000b59eff00308c529c
wh <99289136+wh-Pythonista@users.noreply.github.com>
2025-01-15 16:47:27 +0900

    Update and rename BeautifulSoup_属性へのアクセス.py to 2章/属性へのアクセス.py

---
+++ 2章/属性へのアクセス.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+URL = 'https://pythonscraping.com/pages/page3.html'
+
+response = requests.get(URL)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+img = soup.find('img', {'src': re.compile('\.\./img/gifts/img.*\.jpg')})
+
+print(img.attrs['src'])
+print(img['src'])
