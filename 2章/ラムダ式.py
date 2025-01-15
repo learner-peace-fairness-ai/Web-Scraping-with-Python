cba10f26b0e60dac67fbb75cb7d2408ec486bc30
wh <99289136+wh-Pythonista@users.noreply.github.com>
2025-01-15 16:44:46 +0900

    Update and rename BeautifulSoup_ラムダ式.py to 2章/ラムダ式.py

---
+++ 2章/ラムダ式.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://pythonscraping.com/pages/page3.html'
+
+response = requests.get(URL)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+tag_list = soup.findAll(lambda tag: len(tag.attrs) == 2)
+for tag in tag_list:
+    print(tag)
