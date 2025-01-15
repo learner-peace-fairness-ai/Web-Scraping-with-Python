49076f89c2501497fe8408e7ea51b595ad36e229
wh <99289136+wh-Pythonista@users.noreply.github.com>
2025-01-15 16:41:33 +0900

    Update and rename 正規表現とBeautifulSoup.py to 2章/正規表現とBeautifulSoup.py

---
+++ 2章/正規表現とBeautifulSoup.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+URL = 'https://pythonscraping.com/pages/page3.html'
+
+response = requests.get(URL)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+images = soup.findAll('img', {'src': re.compile('\.\./img/gifts/img.*\.jpg')})
+for img in images:
+    print(img['src'])
