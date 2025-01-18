b54850f7547a365da7280901cc4e07695d41e598
wh <99289136+wh-Pythonista@users.noreply.github.com>
2025-01-18 14:17:27 +0900

    Update articleCrawlSpider.py

--- 3章/wikiSpider/wikiSpider/spiders/articleCrawlSpider.py
+++ 3章/wikiSpider/wikiSpider/spiders/articleCrawlSpider.py
@@ -10,7 +10,6 @@ class ArticleCrawlSpider(CrawlSpider):
     start_urls = ['http://en.wikipedia.org/wiki/Python_%28programming_language%29']
     rules = [Rule(LinkExtractor(allow=(r'(/wiki/)((?!:).)*$')), callback='parse_item', follow=True)]
 
-
     def parse_item(self, response):
         item = Article()
         title = response.xpath('//h1//text()').get()
