0e51329fff13e1b9e9e3c8ce1b221ce1b3dba9e0
Leaner <99289136+learner-peace-fairness-ai@users.noreply.github.com>
2025-04-21 08:38:08 +0900

    Update リダイレクトの処理.py

--- 10章/Ajaxと動的HTML/リダイレクトの処理.py
+++ 10章/Ajaxと動的HTML/リダイレクトの処理.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
